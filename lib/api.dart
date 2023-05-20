223d361b277977b9074961b9e0f2ebd89e12b621
BohdanStarunskyi <bogdan.starunskiy@gmailcom>
2023-05-20 14:06:58 +0300

    fixed timeout, added internet permission for android

--- lib/api.dart
+++ lib/api.dart
@@ -1,6 +1,7 @@
 import 'package:retrofit/http.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:dio/dio.dart';
+import 'package:test_app/comment_service.dart';
 import 'models/comment_model.dart';
 
 @RestApi(baseUrl: "https://jsonplaceholder.typicode.com/")
@@ -13,13 +14,13 @@ abstract class RestClient {
 
 class _RestClient implements RestClient {
   final Dio _dio;
-
   _RestClient(this._dio);
 
   @override
   Future<List<Comment>> getComments() async {
-    final response =
-        await _dio.get('https://jsonplaceholder.typicode.com/comments');
+    dio.options.baseUrl = 'https://jsonplaceholder.typicode.com/';
+    dio.options.connectTimeout = 150000;
+    final response = await _dio.get('comments');
     final List<dynamic> jsonList = response.data;
     final List<Comment> comments =
         jsonList.map((json) => Comment.fromJson(json)).toList();
